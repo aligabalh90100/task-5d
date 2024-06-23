32a4c5a87fa1dafe461a602c41060a745599f54f
aligabalh90100 <aligabalh90100@gmail.com>
2024-06-23 22:05:55 +0300

    update

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ toggleButton.addEventListener("click", () => {
   navButton.classList.toggle("open");
   navListItems.classList.toggle("open");
 });
+const home = document.querySelector("#homelink");
+const skills = document.querySelector("#skilllink");
+const portfolio = document.querySelector("#portfoliolink");
+
+let hash = "/";
+home.classList.add("activeLink");
+window.addEventListener("hashchange", () => {
+  hash = window.location.hash;
+  if (hash == "/" || hash == "#home") {
+    home.classList.add("activeLink");
+    skills.classList.remove("activeLink");
+    portfolio.classList.remove("activeLink");
+  } else if (hash == "#skills") {
+    skills.classList.add("activeLink");
+    home.classList.remove("activeLink");
+    portfolio.classList.remove("activeLink");
+  } else {
+    skills.classList.remove("activeLink");
+    home.classList.remove("activeLink");
+    portfolio.classList.add("activeLink");
+  }
+});
